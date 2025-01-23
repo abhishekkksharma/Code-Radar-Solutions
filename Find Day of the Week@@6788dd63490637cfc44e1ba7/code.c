fe79613828541d7a6834e13238beed0e2f5eb8f7
Abhishek <abhisheksharma7340733@gmail.com>
2025-01-23 14:48:02 +0530

    Solution to Find Day of the Week

--- Find Day of the Week@@6788dd63490637cfc44e1ba7/code.c
+++ Find Day of the Week@@6788dd63490637cfc44e1ba7/code.c
@@ -4,25 +4,18 @@ int welcome(int a) {
     switch (a){
         case 1:
             return "Monday";
-            break;
         case 2:
             return "Tuesday";
-            break;
         case 3:
             return "Wednesday";
-            break;
         case 4:
             return "Thursday";
-            break;
         case 5:
             return "Friday";
-            break;
         case 6:
             return "Saturday";
-            break;
         case 7:
             return "Sunday";
-            break;    
         default:
             return 0;
     }
