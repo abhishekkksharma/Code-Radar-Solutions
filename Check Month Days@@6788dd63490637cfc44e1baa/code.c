d4842d01ce92a7175f7c0ce61491be4862df16c4
Abhishek <abhisheksharma7340733@gmail.com>
2025-01-24 13:26:12 +0530

    Solution to Check Month Days

---
+++ Check Month Days@@6788dd63490637cfc44e1baa/code.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+
+int welcome(int a) {
+    if(a==1){
+        return "Monday";
+    }else if(a==2){
+        return "Tuesday";
+    }else if(a==3){
+        return "Wednesday";
+    }else if(a==4){
+        return "Thursday";
+    }else if(a==5){
+        return "Friday";
+    }else if(a==6){
+        return "Saturday";
+    }else if(a==7){
+        return "Sunday";
+    }
+    return  0;
+}
+
+int main() {
+    int x;
+    scanf("%d",&x);
+    printf("%s", welcome(x));
+    return 0;
+}
