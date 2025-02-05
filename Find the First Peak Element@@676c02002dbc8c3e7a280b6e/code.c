d18d60ff9d7ef7fa699400fd65c65b0c34b7b4be
Abhishek <abhisheksharma7340733@gmail.com>
2025-02-05 10:47:40 +0530

    Solution to Find the First Peak Element

--- Find the First Peak Element@@676c02002dbc8c3e7a280b6e/code.c
+++ Find the First Peak Element@@676c02002dbc8c3e7a280b6e/code.c
@@ -8,7 +8,7 @@ int main() {
     }
     int max=arr[0];
     for(int i=0;i<n;i++){
-        if(arr[i]>arr[i+1]){
+        if(arr[i]>max){
             arr[i]=max;
         }else{
             printf("");
