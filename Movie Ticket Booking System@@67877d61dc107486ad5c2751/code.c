440a25f94b4efb5345886596d4c0599a6ef2a907
Abhishek <abhisheksharma7340733@gmail.com>
2025-02-09 18:58:12 +0530

    Solution to Movie Ticket Booking System

--- Movie Ticket Booking System@@67877d61dc107486ad5c2751/code.c
+++ Movie Ticket Booking System@@67877d61dc107486ad5c2751/code.c
@@ -10,11 +10,11 @@ int main() {
         scanf("%s %s %f",movie_name,sub,&price);
     }
     int rev_standard=0,rev_prem=0,rev_vip=0;
-    if(strcmp(sub=="Standard")){
+    if(strcmp(sub, "Basic")==0){
         rev_standard+=rev;
-    }else if(strcmp(sub=="Premium")){
+    }else if(strcmp(sub,"Premium")==0){
         rev_prem+=rev;
-    }else if(strcmp(sub=="VIP")){
+    }else if(strcmp(sub,"VIP")==0){
         rev_vip+=rev;
     }
     printf("standard: %.2f, Premium: %.2f, VIP: %.2f",rev_standard,rev_prem,rev_vip);
