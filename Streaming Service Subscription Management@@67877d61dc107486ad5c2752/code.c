6afee6dfee2fc35474f630063b6a56bc7222ab7c
Abhishek <abhisheksharma7340733@gmail.com>
2025-02-09 18:48:04 +0530

    Solution to Streaming Service Subscription Management

--- Streaming Service Subscription Management@@67877d61dc107486ad5c2752/code.c
+++ Streaming Service Subscription Management@@67877d61dc107486ad5c2752/code.c
@@ -1,36 +1,33 @@
 #include <stdio.h>
-
+#include <string.h>
 
 int main() {
-    int Basic=0,Standard=0,Premium=0;
-    float rev,rev_basic=0,rev_stan=0,rev_prem=0;
+    int Basic = 0, Standard = 0, Premium = 0;
+    float rev_basic = 0, rev_stan = 0, rev_prem = 0;
     int x;
-    scanf("%d",&x);
-    for(int i=0;i<x;i++){
+    
+    scanf("%d", &x);
+
+    for(int i = 0; i < x; i++) {
         char first_name[10], sub[10];
         float rev;
-        scanf("%s %s %f",first_name,sub,&rev);
-    }
-    for(int i=0;i<n;i++){
-        if(sub==Basic){
+        
+        scanf("%s %s %f", first_name, sub, &rev);
+        
+        if (strcmp(sub, "Basic") == 0) {
             Basic++;
-            rev_basic+=rev;
-
-        }else if(sub==Standard){
+            rev_basic += rev;
+        } else if (strcmp(sub, "Standard") == 0) {
             Standard++;
-            rev_stan+= rev;
-        }else if(sub==Premium){
+            rev_stan += rev;
+        } else if (strcmp(sub, "Premium") == 0) {
             Premium++;
-            rev_prem+=rev;
+            rev_prem += rev;
         }
     }
-    printf("Basic: %d Users, Revenue: %.2f; Standard: %d Users, Revenue: %.2f; Premium: %d Users, Revenue: %.2f",Basic,rev_basic,Standard,rev_stan,Premium,rev_prem)
+
+    printf("Basic: %d Users, Revenue: %.2f; Standard: %d Users, Revenue: %.2f; Premium: %d Users, Revenue: %.2f\n", 
+           Basic, rev_basic, Standard, rev_stan, Premium, rev_prem);
+
     return 0;
 }
-
-
-
-
-
-
-
