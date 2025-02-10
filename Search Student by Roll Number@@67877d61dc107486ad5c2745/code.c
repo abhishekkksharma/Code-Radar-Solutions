1b5c878581c52acd6d98a9b911e8bb01ee951282
Abhishek <abhisheksharma7340733@gmail.com>
2025-02-10 18:38:24 +0530

    Solution to Search Student by Roll Number

--- Search Student by Roll Number@@67877d61dc107486ad5c2745/code.c
+++ Search Student by Roll Number@@67877d61dc107486ad5c2745/code.c
@@ -1,26 +1,25 @@
 #include <stdio.h>
 
-
 int main() {
-    int n,target;
-    scanf("%d",&n);
-    int roll_num[n];
-    char name[n][100];
-    float marks[n];
-    for(int i=0;i<n;i++){
-        scanf("%d %s %f",&roll_num[i],name[i],&marks[i]);
-    }
-    scanf("%d",&target);    
-    for(int i=0;i<n;i++){
-        if(roll_num[i]==target){
-            printf("Roll Number: %d, Name: %s, Marks: %.2f",roll_num[i],name[i],marks[i]);
-            break;
-        } 
-        else if(roll_num[i]!=target){
-            printf("Student not found");
-            break;
-        }
+    int n, target;
 
+    scanf("%d", &n);
+    
+    int roll_num[n];
+    char name[n][10];
+    float marks, Average = 0;
+
+    for(int i = 0; i < n; i++) {
+        scanf("%d %s %f", &roll_num[i], name[i], &marks);
     }
+
+    scanf("%d", &target);
+
+    for(int i = 0; i < n; i++) {
+        if(target == roll_num[i]) {
+            printf("Roll number: %d, Name: %s, Marks: %.2f\n", roll_num[i], name[i], marks);
+        }
+    }
+
     return 0;
 }
