13b30c00b2a419ebb0b754505731c8946cb68726
Abhishek <abhisheksharma7340733@gmail.com>
2025-02-10 19:16:10 +0530

    Solution to Find Student with Minimum Marks

--- Find Student with Minimum Marks@@67877d61dc107486ad5c2749/code.c
+++ Find Student with Minimum Marks@@67877d61dc107486ad5c2749/code.c
@@ -2,23 +2,26 @@
 
 int main() {
     int n;
-    scanf("%d",&n);
+    scanf("%d", &n);
     int roll[n];
     char name[n][100];
-    float marks[n],total,target;;
-    
-    for(int i=0;i<n;i++){
-        scanf("%d %s %f",&roll[i],&name[i],&marks[i]);
+    float marks[n], minMarks;
+    int minIndex = 0;
+
+    for(int i = 0; i < n; i++) {
+        scanf("%d %s %f", &roll[i], name[i], &marks[i]);
     }
-    for(int i=0;i<n;i++){
-        if(n>1){
-            if(marks[i]<marks[i+1] ){
-            printf("Student with Minimum Marks: Roll Number: %d, Name: %s, Marks: %.2f",roll[i],name[i],marks[i]);
+
+    minMarks = marks[0];
+
+    for(int i = 1; i < n; i++) {
+        if(marks[i] < minMarks) {
+            minMarks = marks[i];
+            minIndex = i;
         }
-        }else if(n==1){
-            printf("Student with Minimum Marks: Roll Number: %d, Name: %s, Marks: %.2f",roll[0],name[0],marks[0]);
-        }
-        
     }
+
+    printf("Student with Minimum Marks: Roll Number: %d, Name: %s, Marks: %.2f\n", roll[minIndex], name[minIndex], marks[minIndex]);
+
     return 0;
 }
