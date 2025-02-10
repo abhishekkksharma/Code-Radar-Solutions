335dcae7caab14ef70f791c98c5b519b8f041abf
Abhishek <abhisheksharma7340733@gmail.com>
2025-02-10 19:11:07 +0530

    Solution to Find Student with Minimum Marks

--- Find Student with Minimum Marks@@67877d61dc107486ad5c2749/code.c
+++ Find Student with Minimum Marks@@67877d61dc107486ad5c2749/code.c
@@ -16,7 +16,7 @@ int main() {
             printf("Student with Minimum Marks: Roll Number: %d, Name: %s, Marks: %.2f",roll[i],name[i],marks[i]);
         }
         }
-        else{
+        else if(n==1){
             printf("Student with Minimum Marks: Roll Number: %d, Name: %s, Marks: %.2f",roll[0],name[0],marks[0]);
         }
         
