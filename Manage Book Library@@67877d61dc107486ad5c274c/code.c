39a81a205de94cc50fcceba0d965a43d73a16556
ABHISHEK SHARMA <abhisheksharma7340733@gmail.com>
2025-02-16 12:36:14 +0530

    Solution to Manage Book Library

--- Manage Book Library@@67877d61dc107486ad5c274c/code.c
+++ Manage Book Library@@67877d61dc107486ad5c274c/code.c
@@ -1,1 +1,19 @@
-// Your code here...
+#include <stdio.h>
+
+int main(){
+    int n;
+    scanf("%d",&n);
+    char book_name[n][10], book_auth[n][10];
+    int price[n];
+    for (int i=1;i<=n;i++){
+        scanf("%s %s %d",book_name[i],book_auth[i],&price[i]);
+    } 
+    int target;
+    scanf("%d",&target);
+    for(int i=1;i<=n;i++){
+        if(price[i]>target){
+            printf("Title: %s, Author: %s, Price: %.2f",book_name[i],book_auth[i],price[i])
+        }
+    }
+
+}
