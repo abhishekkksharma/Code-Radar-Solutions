00e4f65b474234f79b6a7dfb2db8bfd8f2bc2b57
ABHISHEK SHARMA <abhisheksharma7340733@gmail.com>
2025-02-16 11:57:59 +0530

    Solution to Toll Plaza Management System

--- Toll Plaza Management System@@67877d61dc107486ad5c2750/code.c
+++ Toll Plaza Management System@@67877d61dc107486ad5c2750/code.c
@@ -6,9 +6,18 @@ int main(){
     char number[n][10];
     char type[n][10];
     int tax[n];
+    int car_tax=0,truck_tax=0,bike_tax=0;
     for(int i=1;i<=n;i++){
-        scanf("%s %s %d",number,type,&tax[i]);
-        printf("%s %s %d\n",number,type,tax[i]);
+        scanf("%s %s %d",number[i],type[i],&tax[i]);
+        if(type[i]=="Car"){
+            car_tax+=tax[i];
+        }else if(type[i]=="Truck"){
+            truck_tax+=tax[i];
+        }else{
+            bike_tax+=tax[i];
+        }
     }
+    printf("Cars: %.2f, Trucks: %.2f, Bikes: %.2f",car_tax,truck_tax,bike_tax);
+
     return 0;
 }
