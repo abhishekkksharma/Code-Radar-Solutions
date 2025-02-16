8b7009d146ba4278ad9dfaa91b4dcdc4002ec1a0
ABHISHEK SHARMA <abhisheksharma7340733@gmail.com>
2025-02-16 11:52:07 +0530

    Solution to Toll Plaza Management System

---
+++ Toll Plaza Management System@@67877d61dc107486ad5c2750/code.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+int main(){
+    int n;
+    scanf("%d",&n);
+    char number[n][10];
+    char type[n][10];
+    float tax[n];
+    for(int i=1;i<=n;i++){
+        scanf("%s %s %f",number,type,&tax);
+        printf("%s %s %f",number,type,tax)
+    }
+    return 0;
+}
