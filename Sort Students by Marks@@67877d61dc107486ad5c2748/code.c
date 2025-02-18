bbf7b60a5e400d6460867820625e10e3850177cc
ABHISHEK SHARMA <abhisheksharma7340733@gmail.com>
2025-02-18 19:52:45 +0530

    Solution to Sort Students by Marks

--- Sort Students by Marks@@67877d61dc107486ad5c2748/code.c
+++ Sort Students by Marks@@67877d61dc107486ad5c2748/code.c
@@ -10,7 +10,7 @@ int main() {
     int max_marks=0;
     for(int i=0;i<n;i++){
         if(marks[i]>max_marks){
-            max_marks=marks[i];
+            marks[i]=max_marks;
             printf("jdhbc %f",max_marks);
         }
         
