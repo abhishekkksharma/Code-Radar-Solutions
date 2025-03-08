237ee0d3395202c4a727924aeacf81c3b9115e39
ABHISHEK SHARMA <abhisheksharma7340733@gmail.com>
2025-03-08 21:22:35 +0530

    Solution to Check if a String is a Palindrome After Removing Spaces

--- Check if a String is a Palindrome After Removing Spaces@@67877c59ddaeab38c8ad25b7/code.c
+++ Check if a String is a Palindrome After Removing Spaces@@67877c59ddaeab38c8ad25b7/code.c
@@ -1,40 +1,28 @@
 #include <stdio.h>
 #include <string.h>
 
-int main() {
+int main(){
     char str[100];
     char newstr[100];
-    int j = 0;
-
-    // Read the input string
-    fgets(str, sizeof(str), stdin);
-
-    // Remove spaces and copy characters to newstr
-    for (int i = 0; i < strlen(str); i++) {
-        if (str[i] != ' ') {
-            newstr[j++] = str[i];  // Copy character and increment j
+    int j=0;
+    fgets(str,sizeof(str),stdin);
+    for(int i=0; i < strlen(str);i++){
+        if(str[i]!=' '){
+            newstr[j++]=str[i];
         }
     }
-    newstr[j] = '\0';  // Null-terminate the new string
-
-    // Remove the newline character if it's present
-    if (newstr[strlen(newstr) - 1] == '\n') {
-        newstr[strlen(newstr) - 1] = '\0';
-    }
-
-    // Check if the string is a palindrome
-    int start = 0;
-    int end = strlen(newstr) - 1;
-
-    while (start < end) {
-        if (newstr[start] != newstr[end]) {
-            printf("No\n");
-            return 0;  // Early exit if not a palindrome
+    newstr[j]='\0';
+    int start=0;
+    int end= strlen(newstr)-1;
+    for(int i=start;i<end;i++){
+        if(newstr[start]==newstr[end]){
+            printf("Yes");
+            break;
+        }else{
+            printf("No");
+            break;
         }
         start++;
         end--;
     }
-
-    printf("Yes\n");  // If no mismatches found, it's a palindrome
-    return 0;
 }
