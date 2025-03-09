fde122ec151975f8fb74c853b1f59091b21a6014
ABHISHEK SHARMA <abhisheksharma7340733@gmail.com>
2025-03-09 21:17:12 +0530

    Solution to Check if a String is Binary

--- Check if a String is Binary@@67877c59ddaeab38c8ad25b6/code.c
+++ Check if a String is Binary@@67877c59ddaeab38c8ad25b6/code.c
@@ -6,7 +6,7 @@ int binarycheck(char a[100]){
     for(int i=1;i<n;i++){
         if(a[i]=='0' || a[i]=='1'){
             return 1;
-        }else if(a==''){
+        }else if(a[i]==''){
             return  1;
         }
     }
