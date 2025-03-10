1cb5fc8661e51f8a5058c4dbb7033e4cae961359
ABHISHEK SHARMA <abhisheksharma7340733@gmail.com>
2025-03-10 09:17:04 +0530

    Solution to Find the Leaders in an Array

--- Find the Leaders in an Array@@676c02002dbc8c3e7a280b7c/code.c
+++ Find the Leaders in an Array@@676c02002dbc8c3e7a280b7c/code.c
@@ -1,16 +1,21 @@
 #include <stdio.h>
-int main(){
+
+int main() {
     int n;
-    scanf("%d",&n);
+    scanf("%d", &n);
     int arr[n];
-    for(int i=0;i<n;i++){
-        scanf("%d",&arr[i]);
+    
+    for (int i = 0; i < n; i++) {
+        scanf("%d", &arr[i]);
     }
-    for(int i=0;i<n-1;i++){
-        if(arr[i]>=arr[i+1]){
-            printf("%d ",arr[i]);
+    
+    for (int i = 0; i < n - 1; i++) {
+        if (arr[i] >= arr[i + 1]) {
+            printf("%d ", arr[i]);
         }
     }
-    printf("%d",arr[n-1]);
+    
+    printf("%d", arr[n - 1]);
+    
     return 0;
 }
