d1b3cdb784c5487bd6f3258f5b0b4f965792168c
ABHISHEK SHARMA <abhisheksharma7340733@gmail.com>
2025-03-11 19:34:43 +0530

    Solution to Find the Element with Maximum Frequency

--- Find the Element with Maximum Frequency@@676c02002dbc8c3e7a280b82/code.c
+++ Find the Element with Maximum Frequency@@676c02002dbc8c3e7a280b82/code.c
@@ -1,22 +1,39 @@
 #include <stdio.h>
 
-int freq(int n,int arr[]){
-    int max=0, count=0;
-    for(int i=0;i<n;i++){
-        for(int j=i+1;j<n;j++){
-            if(arr[i]==arr[j]);
-            return arr[i];
+int freq(int n, int arr[]) {
+    int max_freq = 0;
+    int result = -1; // Stores the element with the maximum frequency
+    
+    for(int i = 0; i < n; i++) {
+        int count = 0;
+        // Count the frequency of arr[i]
+        for(int j = 0; j < n; j++) {
+            if(arr[i] == arr[j]) {
+                count++;
+            }
         }
-    }    
-    return 0;
-}
-int main(){
-    int n;
-    scanf("%d",&n);
-    int arr[n];
-    for(int i=0;i<n;i++){
-        scanf("%d",&arr[i]);
+        
+        // If count of current element is greater than max_freq, update
+        if(count > max_freq) {
+            max_freq = count;
+            result = arr[i];
+        }
     }
-    printf("%d",freq(n,arr));
+    
+    return result; // Return the element with the maximum frequency
+}
+
+int main() {
+    int n;
+    scanf("%d", &n);
+    int arr[n];
+    
+    for(int i = 0; i < n; i++) {
+        scanf("%d", &arr[i]);
+    }
+
+    int result = freq(n, arr);
+    printf("%d", result);
+
     return 0;
 }
