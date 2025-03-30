44b0b31a7733421de78d924cf11792d6a1b73427
ABHISHEK SHARMA <abhisheksharma7340733@gmail.com>
2025-03-30 18:06:06 +0530

    Solution to Bubble Sort Implementation

--- Bubble Sort Implementation@@67a4d9012ee20af0252bc6cb/code.c
+++ Bubble Sort Implementation@@67a4d9012ee20af0252bc6cb/code.c
@@ -1,57 +1,32 @@
 #include <stdio.h>
-
-// Function to perform bubble sort
-void bubbleSort(int arr[], int n) {
-    for (int i = 0; i < n-1; i++) {
-        // Flag to check if any swapping happened
-        int swapped = 0;
-        
-        for (int j = 0; j < n-i-1; j++) {
-            // Swap if the element found is greater than the next element
-            if (arr[j] > arr[j+1]) {
-                // Swap the elements
-                int temp = arr[j];
-                arr[j] = arr[j+1];
-                arr[j+1] = temp;
-                swapped = 1;
+void bubblesort(int arr[], int size){
+    for (int i = 0; i < size-1; i++){
+        for (int j = 0; j < size-i-1; j++)
+        {
+            if(arr[j]>arr[j+1]){
+                int temp=arr[j];
+                arr[j]=arr[j+1];
+                arr[j+1]=temp;
             }
-        }
-
-        // If no two elements were swapped in the inner loop, break
-        if (swapped == 0)
-            break;
+        }        
     }
+    
 }
-
-// Function to print an array
-void printArray(int arr[], int n) {
-    for (int i = 0; i < n; i++) {
-        printf("%d ", arr[i]);
+int main() {
+    int size;
+    printf("Enter the size of the array : ");
+    scanf("%d",&size);
+    int arr[size];
+    printf("Enter the array: ");
+    for (int i = 0; i < size; i++)
+    {
+        scanf("%d",&arr[i]);
     }
-    printf("\n");
+    bubblesort(arr,size);
+    for (int i = 0; i < size; i++)
+    {
+        printf("%d ",arr[i]);
+    }
+    
+    return 0;
 }
-
-// int main() {
-//     int n;
-    
-//     // Take input for the size of the array
-//     scanf("%d", &n);
-    
-//     int arr[n];
-
-//     // Take input for the array elements
-//     for (int i = 0; i < n; i++) {
-//         scanf("%d", &arr[i]);
-//     }
-
-//     // Display the original array
-//     printArray(arr, n);
-
-//     // Perform bubble sort
-//     bubbleSort(arr, n);
-
-//     // Display the sorted array
-//     printArray(arr, n);
-
-//     return 0;
-// }
