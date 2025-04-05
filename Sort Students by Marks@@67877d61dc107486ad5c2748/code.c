884bbcfddea35bb5c55b4fa4e8bffabbb064c316
ABHISHEK SHARMA <abhisheksharma7340733@gmail.com>
2025-04-05 22:03:45 +0530

    Solution to Sort Students by Marks

--- Sort Students by Marks@@67877d61dc107486ad5c2748/code.c
+++ Sort Students by Marks@@67877d61dc107486ad5c2748/code.c
@@ -1,5 +1,5 @@
 #include <stdio.h>
-void bubblesort(int roll_num[],char name[],float marks[],int n){
+void bubblesort(int roll_num[], char name[][50], float marks[], int n)
     for(int i=0;i<n;i++){
         for(int j=0;j<n-i-1;j++){
             if(marks[j]>marks[j+1]){
